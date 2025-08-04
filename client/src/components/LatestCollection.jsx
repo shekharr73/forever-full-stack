1f4c1fa62cd734fad82007e06e8fc04f8d21c831
shekharr.73 <shekharjadhav501@gmail.com>
2025-08-04 17:05:50 +0530

    initila commit

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -15,7 +15,7 @@ const LatestCollection = () => {
     <div className="my-10">
       <div className="text-center py-8 text-3xl ">
         <Title text1={"LATEST"} text2={"COLLECTIONS"} />
-        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non doloribus ut consequuntur velit.</p>
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Discover our latest arrivals made to elevate your everyday style. Explore now and find your perfect look!</p>
       </div>
 
       {/* Rendering Products */}
